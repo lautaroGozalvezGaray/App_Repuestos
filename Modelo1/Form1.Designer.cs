f788f0473958250ca28b561e6fbb4376d7fc4c79
LautaroGozalvezGaray <lautarogozalvez@gmail.com>
2022-10-01 02:59:02 -0300

    creando repositorio para App

---
+++ Modelo1/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace Parcial2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.cmbOrigen = new System.Windows.Forms.ComboBox();
+            this.cmbMarca = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.dgvRepuestos = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRepuestos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRegistrar);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.txtDescripcion);
+            this.groupBox1.Controls.Add(this.txtNumero);
+            this.groupBox1.Controls.Add(this.cmbOrigen);
+            this.groupBox1.Controls.Add(this.cmbMarca);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(625, 271);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registro de Repuestos";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(475, 32);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(105, 33);
+            this.btnRegistrar.TabIndex = 10;
+            this.btnRegistrar.Text = "&Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(151, 218);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(115, 22);
+            this.txtPrecio.TabIndex = 9;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(151, 172);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(387, 22);
+            this.txtDescripcion.TabIndex = 8;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(151, 127);
+            this.txtNumero.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(132, 22);
+            this.txtNumero.TabIndex = 7;
+            // 
+            // cmbOrigen
+            // 
+            this.cmbOrigen.FormattingEnabled = true;
+            this.cmbOrigen.Items.AddRange(new object[] {
+            "Nacional",
+            "Importado",
+            ""});
+            this.cmbOrigen.Location = new System.Drawing.Point(151, 81);
+            this.cmbOrigen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbOrigen.Name = "cmbOrigen";
+            this.cmbOrigen.Size = new System.Drawing.Size(132, 24);
+            this.cmbOrigen.TabIndex = 6;
+            // 
+            // cmbMarca
+            // 
+            this.cmbMarca.FormattingEnabled = true;
+            this.cmbMarca.Location = new System.Drawing.Point(151, 36);
+            this.cmbMarca.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbMarca.Name = "cmbMarca";
+            this.cmbMarca.Size = new System.Drawing.Size(160, 24);
+            this.cmbMarca.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 222);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Precio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 176);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Descripción";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 130);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Número";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 85);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Origen";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Marca";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtTotal);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.btnConsultar);
+            this.groupBox2.Controls.Add(this.dgvRepuestos);
+            this.groupBox2.Location = new System.Drawing.Point(16, 308);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(625, 342);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Consulta de Repuestos";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(431, 302);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(132, 22);
+            this.txtTotal.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(381, 305);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 16);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Total";
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(475, 25);
+            this.btnConsultar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(105, 33);
+            this.btnConsultar.TabIndex = 1;
+            this.btnConsultar.Text = "&Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // dgvRepuestos
+            // 
+            this.dgvRepuestos.AllowUserToAddRows = false;
+            this.dgvRepuestos.AllowUserToDeleteRows = false;
+            this.dgvRepuestos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRepuestos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvRepuestos.Location = new System.Drawing.Point(8, 80);
+            this.dgvRepuestos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvRepuestos.Name = "dgvRepuestos";
+            this.dgvRepuestos.ReadOnly = true;
+            this.dgvRepuestos.RowHeadersVisible = false;
+            this.dgvRepuestos.RowHeadersWidth = 51;
+            this.dgvRepuestos.Size = new System.Drawing.Size(592, 217);
+            this.dgvRepuestos.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Código";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 110;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Descripción";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 210;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Precio";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 125;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(475, 657);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(105, 33);
+            this.btnCerrar.TabIndex = 2;
+            this.btnCerrar.Text = "C&errar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 711);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Repuestos AutoRep";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRepuestos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.ComboBox cmbOrigen;
+        private System.Windows.Forms.ComboBox cmbMarca;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.DataGridView dgvRepuestos;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Modelo1/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using System.Globalization;
+
+namespace Parcial2
+{
+    public partial class Form1 : Form
+    {
+
+        // objetos privados para compartir en este formulario
+        private DataSet ds;
+        private OleDbDataAdapter daR; //para la tabla Repuestos
+        private OleDbDataAdapter daM; //para la tabla Marcas
+        private const string PATH_ARCHIVO = "ReporteRepuesto.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //conexion a la DB
+
+            OleDbConnection cnn = new OleDbConnection();
+            cnn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=" + Application.StartupPath + "\\AutoRep.mdb";
+            cnn.Open();
+
+            //configuracion para la tabla Repuestos
+
+            OleDbCommand cmR = new OleDbCommand();
+            cmR.Connection = cnn;
+            cmR.CommandType = CommandType.TableDirect;
+            cmR.CommandText = "Repuestos";
+
+            daR = new OleDbDataAdapter(cmR);
+
+            ds = new DataSet();
+
+            daR.Fill(ds, "Repuestos");
+
+            OleDbCommandBuilder cbR = new OleDbCommandBuilder(daR);
+
+
+            //Crear comando para usar la tabla marcas
+
+            OleDbCommand cmM = new OleDbCommand();
+            cmM.Connection = cnn;
+            cmM.CommandType = CommandType.TableDirect;
+            cmM.CommandText = "Marcas";
+
+            daM = new OleDbDataAdapter(cmM);
+
+            daM.Fill(ds, "Marcas");
+
+            OleDbCommandBuilder cbM = new OleDbCommandBuilder(daM);
+
+            //definimos la clave primaria para la tabla Marcas
+
+            DataColumn[] dcM = new DataColumn[1];
+            dcM[0] = ds.Tables["Marcas"].Columns["IdMarca"];
+            ds.Tables["Marcas"].PrimaryKey = dcM;
+
+            cmbMarca.DisplayMember = "Nombre";
+            cmbMarca.ValueMember = "IdMarca";
+            cmbMarca.DataSource = ds.Tables["Marcas"];
+
+
+
+
+            // para cargar los campos vacios
+
+            txtDescripcion.Text = "";
+            txtNumero.Text = "";
+            txtPrecio.Text = "";
+
+        }
+
+
+        //metodo para validar los campos no esten vacios y para validar que el numero de repuesto no se repita
+        private bool validar()
+        {
+            bool resultado = false;
+
+            if(txtNumero.Text != "" && txtPrecio.Text != "" && txtDescripcion.Text != "" && cmbOrigen.SelectedIndex != -1)
+            {
+
+                
+
+                if(ds.Tables["Repuestos"].Rows.Count < 1)
+                {
+                    resultado = true;
+                }
+                else
+                {
+                    foreach (DataRow dr in ds.Tables["Repuestos"].Rows)
+                    {
+                        if ((int)dr["Numero"] == int.Parse(txtNumero.Text))
+                        {
+                            MessageBox.Show("El numero de repuesto ya existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            resultado = true;
+                        }
+                    }
+                }
+            }
+
+            return resultado;
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if(validar() == true)
+            {
+                //configuramos para agregar un nuevo registro
+
+                DataRow dr = ds.Tables["Repuestos"].NewRow();
+
+                dr["Numero"] = txtNumero.Text;
+                dr["Descripcion"] = txtDescripcion.Text;
+                dr["Precio"] = int.Parse(txtPrecio.Text);
+                dr["IdMarca"] = cmbMarca.SelectedIndex;
+                dr["Origen"] = cmbOrigen.GetItemText(cmbOrigen.SelectedItem);
+
+                //agregamos el nuevo registro a la tabla
+
+                ds.Tables["Repuestos"].Rows.Add(dr);
+
+                //actualizamos la tabla en la DB
+
+                daR.Update(ds, "Repuestos");
+
+                //generacion de reporte y grabado en archivo txt
+
+                if (PATH_ARCHIVO != "")
+                {
+                    // crea el stream en modo append (para agregar sin borrar el contenido del archivo)
+                    StreamWriter sw = new StreamWriter(PATH_ARCHIVO, true);
+                    // graba la linea con los valores de los campos
+                    sw.WriteLine(txtNumero.Text.ToString() + "," + txtDescripcion.Text + "," +
+                        decimal.Parse(txtPrecio.Text).ToString("#.00", CultureInfo.InvariantCulture) + "," +
+                        cmbOrigen.GetItemText(cmbOrigen.SelectedItem)  + ","+  cmbMarca.GetItemText(cmbMarca.SelectedItem));
+                    sw.Close();  // cerrar el stream
+                    sw.Dispose(); // liberar los recursos
+                    
+                }
+
+
+
+                txtDescripcion.Text = "";
+                txtNumero.Text = "";
+                txtPrecio.Text = "";
+            }
+            else{
+                MessageBox.Show("Datos incorrectos o Faltanes", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //para mostrar los datos en el dataGridView
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+
+            dgvRepuestos.Rows.Clear();
+
+            decimal total = 0;
+
+            if (ds.Tables["Repuestos"].Rows.Count > 0)
+            {
+                //foreach para recorrer la tabla y tomar los valores que necesitamos
+
+                foreach(DataRow dr in ds.Tables["Repuestos"].Rows)
+                {
+                    dgvRepuestos.Rows.Add(dr["Numero"], dr["Descripcion"], dr["Precio"]);
+
+                    total += decimal.Parse(dr["Precio"].ToString());
+                }
+            }
+
+            txtTotal.Text = total.ToString();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
